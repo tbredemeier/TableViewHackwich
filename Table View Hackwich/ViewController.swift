6e7207c11cb9c4ecf4c242e13ea1cbb5ce3941ed
Tom Bredemeier <tbredemeier@barrington220.org>
2016-01-13 10:26:14 -0600

    Connect Storyboard and added protocols

--- Table View Hackwich/ViewController.swift
+++ Table View Hackwich/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var cities = ["Chicago", "Denver", "Seattle"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
