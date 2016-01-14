18490698a05b49c93f136c42d59427a286088363
Tom Bredemeier <tbredemeier@barrington220.org>
2016-01-13 21:51:26 -0600

    Allow for entries to be re-ordered

--- Table View Hackwich/ViewController.swift
+++ Table View Hackwich/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
     var cities = ["Chicago", "Denver", "Seattle"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        editButton.tag = 0
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,6 +35,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             tableView.reloadData()
         }
     }
+
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let city = cities[sourceIndexPath.row]
+        cities.removeAtIndex(sourceIndexPath.row)
+        cities.insert(city, atIndex: destinationIndexPath.row)
+    }
+
     @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add City", message: nil, preferredStyle: .Alert)
         alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
@@ -47,6 +60,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         alert.addAction(addAction)
         self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func onTappedEditButton(sender: UIBarButtonItem) {
+        if sender.tag == 0 {
+            tableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            tableView.editing = false
+            sender.tag = 0
+        }
     }
 }
 
